4df99892598c9cda35449dbaeb55e0b201d78b41
anmvk58 <an.cnttk58@gmail.com>
2022-10-29 16:55:28 +0700

    20221029

--- src/main/java/com/anmv/backend/GroupRepository.java
+++ src/main/java/com/anmv/backend/GroupRepository.java
@@ -5,6 +5,7 @@ import com.anmv.utils.HibernateUtils;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public class GroupRepository {
@@ -14,6 +15,23 @@ public class GroupRepository {
         this.hibernateUtils = HibernateUtils.getInstance();
     }
 
+    // Phương thức tạo 1 Group mới
+    public void createGroup(Group group){
+        Session session = null;
+        try{
+            session = hibernateUtils.openSession();
+            session.save(group);
+            System.out.println("Thêm mới Group thành công");
+        } catch (Exception e){
+            System.out.println("Thêm mới thất bại :" + e.getMessage());
+        } finally {
+            if(session != null){
+                session.close();
+            }
+        }
+    }
+
+    // Phương thức lấy tất cả Group
     public List<Group> getListGroup(){
         Session session = null;
         try{
@@ -21,6 +39,57 @@ public class GroupRepository {
             Query<Group> listGroups = session.createQuery("from Group");
 
             return listGroups.list();
+        } finally {
+            if(session != null){
+                session.close();
+            }
+        }
+    }
+
+    // Phương thức lấy 1 Group dựa vào ID
+    public Group getGroupById(short id){
+        Session session = null;
+        try{
+            session = hibernateUtils.openSession();
+            Group group = session.get(Group.class, id);
+
+            return group;
+        } finally {
+            if(session != null){
+                session.close();
+            }
+        }
+    }
+
+    // Phương thức lấy 1 Group dựa vào name:
+    public List<Group> getGroupByName(String name){
+        Session session = null;
+        try{
+            session = hibernateUtils.openSession();
+            Query<Group> query = session.createQuery("FROM Group WHERE groupName = :nameParameter");
+
+            query.setParameter("nameParameter", name);
+
+            return query.list();
+        } finally {
+            if(session != null){
+                session.close();
+            }
+        }
+    }
+
+    // Phương thức dùng để update Groups:
+    public void updateGroup(short id, String newName){
+        Session session = null;
+        try{
+            session = hibernateUtils.openSession();
+            session.beginTransaction();
+
+            Group group = session.load(Group.class, id);
+
+            group.setGroupName(newName);
+
+            session.getTransaction().commit();
         } finally {
             if(session != null){
                 session.close();
--- src/main/java/com/anmv/frontend/GroupProgram.java
+++ src/main/java/com/anmv/frontend/GroupProgram.java
@@ -3,14 +3,40 @@ package com.anmv.frontend;
 import com.anmv.backend.GroupRepository;
 import com.anmv.entity.Group;
 
+import java.util.Date;
 import java.util.List;
 
 public class GroupProgram {
     public static void main(String[] args) {
         GroupRepository groupRepository = new GroupRepository();
+
+        // Create new group
+//        System.out.println("Create a new Group");
+//        Group group = new Group();
+//        group.setGroupName("Test Group");
+//        group.setCreateDate(new Date());
+//        groupRepository.createGroup(group);
+
+        // View all Group
+        System.out.println("\nView all group in tables: ");
         List<Group> listGroups = groupRepository.getListGroup();
         for(Group g : listGroups){
             System.out.println(g);
         }
+
+        // Get a Group by ID:
+        System.out.println("\nGet Group by ID:");
+        Group group1 = groupRepository.getGroupById((short) 3);
+        System.out.println(group1);
+
+        // Get a Group by Name
+        System.out.println("\nGet Group by Name:");
+        List<Group> listGroups2 = groupRepository.getGroupByName("VTI Sale 01");
+        for(Group g : listGroups2){
+            System.out.println(g);
+        }
+
+        // Update a Group:
+        groupRepository.updateGroup((short) 11, "new Name");
     }
 }
